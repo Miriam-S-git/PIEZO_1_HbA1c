63bd01f5b23b7cda5aef38cec727f95c5aec3fc2
Miriam Samuel <74236842+Miriam-S-git@users.noreply.github.com>
2024-10-21 15:37:54 +0100

    Update 2_clean_medication_data.R

--- scripts/2_clean_medication_data.R
+++ scripts/2_clean_medication_data.R
@@ -27,6 +27,34 @@ dplyr::rename ("bnf_section" = 3,
 dplyr::filter(bnf_paragraph code == "060101"|bnf_paragraph code =="060102")  # filter diabetes drug names
 
 
+# read in mapping file and harmonise column names
+#bnf_snomed_mapping_data <- read_csv ("PATH")
+
+
+bnf_snomed_mapping <- bnf_snomed_mapping_data %>% 
+dplyr::rename ("bnf_code" = 3, 
+               "snomed_code = 5 %>% 
+dplyr::select(bnf_code, snomed_code)
+
+# join snomed codes to diabetes drugs. 
+# Is many to one as multiple SNOMED codes for a single bnf code
+
+diabetes_drugs <- diabetes_drugs %>% 
+dplyr::left_join (
+bnf_snomed_mapping, 
+by = join_by (bnf_code)
+)
+
+# save a copy
+# write_csv(diabetes_drugs, "PATH/diabetes_drugs.csv")
+
+
+################## PART 2:LINK SNOMED codes to patient records
+
+
+
+
+
 
 
 
